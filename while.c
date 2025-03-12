d95c11909df0f15b4f5fb3be3a3b80f51e449c93
Anderson G. Galvão <andersonggalvao09@gmail.com>
2025-03-12 19:51:02 +0000

    while

---
+++ while.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+    int i = 1; //variável de incremento
+    while (i <= 35) //condição
+    {
+        printf("%d\n", i); //saída
+        i ++; //incremento
+    }
+    
+}
